648c9722d2939a625e66434baf78065ea9fdd553
Norton <PNorton@hartreepartners.com>
2021-09-30 15:27:51 +0100

    refactor to use curve id instead of symbols

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from hdq_utils import getHdqPbAsDF
-from metadata import metadata
-from constants import CURVE_ID, OBSERVATION_DATE, CONTRACT_START, VALUE, EXPIRY_DATE, DAYS_FROM_EXPIRY, hdq_expression
+from metadata import symbol_metadata, curve_metadata
+from constants import CURVE_ID, OBSERVATION_DATE, CONTRACT_START, VALUE, EXPIRY_DATE, DAYS_FROM_EXPIRY, SYMBOLS, SYMBOL, \
+    LEGEND, hdq_expression
 
 
 config_rbob_ebob_builtup_arb = [
@@ -20,9 +21,9 @@ def clean_hdq_timeseries(df, symbol):
     df.set_index([OBSERVATION_DATE], drop=True, inplace=True)
     # value
     df.columns = [VALUE]
-    # curve_id
-    df[CURVE_ID] = symbol
-    df.set_index(CURVE_ID, drop=True, append=True, inplace=True)
+    # symbol
+    df[SYMBOL] = symbol
+    df.set_index(SYMBOL, drop=True, append=True, inplace=True)
     return df
 
 
@@ -33,44 +34,72 @@ def add_forward_date_to_hdq_timeseries(df, metadata_df):
 
 
 def clean_hdq_expiry(df, symbol):
-    df.columns = ['expiry_date']
+    df.columns = [EXPIRY_DATE]
     df.index = [symbol]
+    df.index.name = SYMBOL
     return df
 
 
-def build_ts_df(symbols, metadata):
-    metadata_df = pd.DataFrame.from_dict(metadata, orient='index')
-    df_ts = pd.DataFrame()
-    for symbol in symbols:
+def build_single_curve_df(curve_id, curve_metadata, symbol_metadata):
+    curve_metadata = curve_metadata[curve_id]
+    df_curve = pd.DataFrame()
+    for symbol in curve_metadata[SYMBOLS]:
+        # call hdq api
         method = 'TimeSeries'
         kwargs = {'method': method,
                   'symbol': symbol}
         df = getHdqPbAsDF(expression=hdq_expression.format(**kwargs))
+        # rename columns and set index
         df = clean_hdq_timeseries(df, symbol)
-        df = add_forward_date_to_hdq_timeseries(df, metadata_df)
-        df_ts = pd.concat([df_ts, df], axis='rows')
-    return df_ts
+        # add the contract start date from our metadata then move to the index
+        df[CONTRACT_START] = symbol_metadata[symbol][CONTRACT_START]
+        df.set_index(CONTRACT_START, drop=True, append=True, inplace=True)
+        # add the curve_id as a column then move to the index
+        df[CURVE_ID] = curve_id
+        df.set_index(CURVE_ID, drop=True, append=True, inplace=True)
+        # stack the df from each symbol
+        df_curve = pd.concat([df_curve, df], axis='rows')
+    # now we can remove the symbol
+    df_curve.index = df_curve.index.droplevel(SYMBOL)
+    # and reorder
+    df_curve = df_curve.reorder_levels([CURVE_ID, CONTRACT_START, OBSERVATION_DATE], axis='index')
+    return df_curve
 
 
-def get_hdq_expiries(symbols):
+def get_expiries_for_single_curve(curve_id, curve_metadata, symbol_metadata):
+    curve_metadata = curve_metadata[curve_id]
     df_expiries = pd.DataFrame()
-    for symbol in symbols:
+    for symbol in curve_metadata[SYMBOLS]:
         method = 'FutureExpiry'
         kwargs = {'method': method,
                   'symbol': symbol}
         df = getHdqPbAsDF(expression=hdq_expression.format(**kwargs))
         df = clean_hdq_expiry(df, symbol=symbol)
+        # add the contract start date from our metadata then move it to the index
+        df[CONTRACT_START] = symbol_metadata[symbol][CONTRACT_START]
+        df.set_index(CONTRACT_START, drop=True, append=True, inplace=True)
+        # add the curve_id as a column then move to the index
+        df[CURVE_ID] = curve_id
+        df.set_index(CURVE_ID, drop=True, append=True, inplace=True)
+        # now we can remove the symbol
+        df.index = df.index.droplevel(SYMBOL)
+        # stack the df from each symbol
         df_expiries = pd.concat([df_expiries, df], axis='rows')
+    # and reorder
+    df_expiries = df_expiries.reorder_levels([CURVE_ID, CONTRACT_START], axis='index')
     return df_expiries
 
 
 def enrich_ts(df, metadata_df):
-    return pd.merge(df, metadata_df, left_on=CURVE_ID, right_index=True)
+    # horrific pandas join hack
+    # https://pandas.pydata.org/pandas-docs/version/0.24.0/user_guide/merging.html
+    df = pd.merge(df.reset_index(), metadata_df.reset_index(), left_on=(CURVE_ID, CONTRACT_START), right_on=(CURVE_ID, CONTRACT_START))
+    df.set_index([CURVE_ID, OBSERVATION_DATE, CONTRACT_START], drop=True, inplace=True)
+    return df
 
 
 def calc_days_from_expiry(df):
     df[DAYS_FROM_EXPIRY] = df.index.get_level_values(OBSERVATION_DATE) - df[EXPIRY_DATE]
-    return df
 
 
 def reset_index_for_chart_data(df, index_columns):
@@ -84,28 +113,50 @@ def reset_index_for_chart_data(df, index_columns):
 def pivot_for_chart_data(s, chart_index):
     df = s.to_frame()
     df.reset_index(inplace=True)
-    df = df.pivot(index=chart_index, columns=CURVE_ID)
+    df = df[[chart_index, LEGEND, VALUE]]
+    df = df.pivot(index=chart_index, columns=LEGEND)
     # the value column name is added to the column index
     # remove it here
     df.columns = df.columns.droplevel(None)
     return df
 
 
+def create_legend_index_for_chart_data(df, legends_mapper, names):
+    mapper_index = pd.MultiIndex.from_tuples(zip(*legends_mapper))
+    mapper_index.names = names
+    return df.index.join(mapper_index)
+
+
+def add_index_to_df(df, new_index):
+    df.index = new_index
+    return df
+
+
 if __name__ == '__main__':
-    symbols = ['IPEBRT19Z', 'IPEBRT20Z', 'IPEBRT21Z']
+    curve_id = 1000000
+    legends_mapper = [pd.DatetimeIndex(['2019-12-01', '2020-12-01', '2021-12-01']),
+                      ['Dec2019', 'Dec2020', 'Dec2021']]
+    legends_names = [CONTRACT_START, LEGEND]
 
-    df_expiries = get_hdq_expiries(symbols=symbols)
+    # call hdq api for all symbols that are members of this curve_id
+    # returns a df of expiries
+    df_expiries = get_expiries_for_single_curve(curve_id=curve_id, curve_metadata=curve_metadata, symbol_metadata=symbol_metadata)
 
-    df_ts = build_ts_df(symbols=symbols, metadata=metadata)
+    # call hdq api for all symbols that are members of this curve_id
+    # returns the correct timeseries structure for a forward curve which can be stacked
+    df_ts = build_single_curve_df(curve_id=curve_id, curve_metadata=curve_metadata, symbol_metadata=symbol_metadata)
 
     # enrich with expiry dates
     df_ts_enrich = enrich_ts(df_ts, df_expiries)
-
-    # calculate days from expiry
-    df = calc_days_from_expiry(df_ts_enrich)
-
+    # and then calc days from expiry
+    calc_days_from_expiry(df_ts_enrich)
+    
     # make days_from_expiry the new index
-    df = reset_index_for_chart_data(df, [DAYS_FROM_EXPIRY])
+    df = reset_index_for_chart_data(df_ts_enrich, [CONTRACT_START, DAYS_FROM_EXPIRY])
+
+    # add legend to dataframe
+    index = create_legend_index_for_chart_data(df, legends_mapper, legends_names)
+    df = add_index_to_df(df, index)
 
     # pivot the data ready to chart it
     df = pivot_for_chart_data(df, chart_index=DAYS_FROM_EXPIRY)
--- metadata.py
+++ metadata.py
@@ -1,26 +1,23 @@
 import datetime as dt
-from constants import CONTRACT_START, LONG_SCREEN_NAME, UOM, CURRENCY, EXPIRY_DATE
+from constants import CONTRACT_START, LONG_SCREEN_NAME, UOM, CURRENCY
 
-metadata = {'IPEBRT19Z': {
-    CONTRACT_START: dt.date(2019, 12, 1),
-    LONG_SCREEN_NAME: 'IPE Brent Futures Dec 19',
-    CURRENCY: 'USD',
-    UOM: 'barrel',
-    EXPIRY_DATE: dt.date(2019, 10, 31)  # IPEBRT19Z  2019-10-31
-},
-'IPEBRT20Z': {
-    CONTRACT_START: dt.date(2020, 12, 1),
-    LONG_SCREEN_NAME: 'IPE Brent Futures Dec 20',
-    CURRENCY: 'USD',
-    UOM: 'barrel',
-    EXPIRY_DATE: dt.date(2020, 10, 30)  # IPEBRT20Z  2020-10-30
-},
-'IPEBRT21Z': {
-    CONTRACT_START: dt.date(2021, 12, 1),
-    LONG_SCREEN_NAME: 'IPE Brent Futures Dec 21',
-    CURRENCY: 'USD',
-    UOM: 'barrel',
-    EXPIRY_DATE: dt.date(2021, 10, 29)  # IPEBRT21Z  2021-10-29
-}
+symbol_metadata = {
+    'IPEBRT19Z': {CONTRACT_START: dt.date(2019, 12, 1)},
+    'IPEBRT20Z': {CONTRACT_START: dt.date(2020, 12, 1)},
+    'IPEBRT21Z': {CONTRACT_START: dt.date(2021, 12, 1)}
 }
 
+curve_metadata = {
+    1000000: {
+        'symbols': ['IPEBRT19Z', 'IPEBRT20Z', 'IPEBRT21Z'],
+        'names': {LONG_SCREEN_NAME: 'IPE Brent Futures'},
+        UOM: 'bbl',
+        CURRENCY: 'USD'
+    },
+    1000001: {
+        'symbols': ['CMEWTI19Z', 'CMEWTI20Z', 'CMEWTI21Z'],
+        'names': {LONG_SCREEN_NAME: 'CME WTI Futures'},
+        UOM: 'bbl',
+        CURRENCY: 'USD'
+    },
+}
