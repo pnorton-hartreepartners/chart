9461f300a1b201bdc592c740e2b7955d33281f0a
Norton <PNorton@hartreepartners.com>
2021-12-13 11:21:14 +0000

    tidying

--- correlation_analysis.py
+++ correlation_analysis.py
@@ -2,11 +2,11 @@ import pandas as pd
 import numpy as np
 import os
 from pprint import pprint as pp
-import itertools
-
 from mosaic.constants import DEV, path
 from mosaic.mosaic_api_templates import api_config_dict
 from mosaic.mosaic_wapi import build_partial_url_kwargs, build_url, post_any_api, process_chart_data
+
+file_for_df = 'correlation.pkl'
 
 trader_curves = [('BRT-F', 'Dynamic'),
                  ('EBOB-S', 'Combo'),
@@ -36,115 +36,120 @@ expression_template_dict = {
 }
 
 
-def build_timespread_payload(expression, type, start='2021-01-01', periods=13):
+def _build_timespreads(start, periods):
     months = pd.date_range(start=start, periods=periods, freq='MS')
     months = months.strftime('%Y%m')
-    contracts = list(zip(months[:-1], months[1:]))
+    return list(zip(months[:-1], months[1:]))
 
-    expression_template_dict['expression'] = expression
-    expression_template_dict['type'] = type
 
-    # each spread will be a chartlet
+def _build_timespread_chartlet_name(product, contract_list):
+    front, back = contract_list
+    return product + ' | ' + front + ' minus ' + back
+
+
+def _build_chartlets(expression, type_, contracts, name_func):
+    # each contract will be a chartlet ie a line on the chart
     chartlet_list = []
-    for contract in contracts:
-        front, back = contract
-        expression_dict = expression_template_dict.copy()
-        expression_dict['contracts'] = [front, back]
-
+    for contract_list in contracts:
+        # chartlet_dict contains curves_dict
         chartlet_dict = chartlet_template_dict.copy()
-        chartlet_dict['name'] = expression + ' | ' + front + ' minus ' + back
-        chartlet_dict['curves'] = [expression_dict]
+        curves_dict = expression_template_dict.copy()
+
+        # update values
+        chartlet_dict['name'] = name_func(expression, contract_list)
+        curves_dict['expression'] = expression
+        curves_dict['type'] = type_
+        curves_dict['contracts'] = list(contract_list)
+        chartlet_dict['curves'] = [curves_dict]
+
         chartlet_list.append(chartlet_dict)
-    chart_template_dict['chartlets'] = chartlet_list
-    pp(chart_template_dict)
-    return chart_template_dict
+    return chartlet_list
 
 
-def build_and_save_clean_data(trader_curves):
+def collect_and_build_clean_data(trader_curves, start, periods):
     api_name = 'getTraderCurveTS'
+    all_df = pd.DataFrame()
     for expression, type_ in trader_curves:
-        payload = build_timespread_payload(expression=expression, type=type_, start='2021-01-01', periods=13)
-
+        # build url
         template_url = api_config_dict[api_name]['url_template']
         url_kwargs = build_partial_url_kwargs(api_name, env=env)
         url = build_url(template_url=template_url, kwargs=url_kwargs)
 
+        # build payload
+        contracts = _build_timespreads(start=start, periods=periods)
+        chartlets = _build_chartlets(expression=expression, type_=type_,
+                                     contracts=contracts,
+                                     name_func=_build_timespread_chartlet_name)
+        chart_template_dict['chartlets'] = chartlets
+
         # call the api
-        result = post_any_api(url, payload=payload)
+        result = post_any_api(url, payload=chart_template_dict)
 
-        # process result and return a df if possible; but it will be pivotted
-        df = process_chart_data(result, already_pivotted=True)
+        # process the result
+        df = clean_api_result(result)
 
-        # so unpivot the data
-        norm_df = pd.melt(df, value_vars=df.columns, ignore_index=False)
+        # one big df with all the lovely data
+        all_df = pd.concat([all_df, df], axis='index')
 
-        # clean it
-        norm_df[['symbol', 'contract']] = norm_df['variable'].str.split(pat='|').apply(
-            lambda x: [e.strip() for e in x]).tolist()
-        norm_df.drop(columns='variable', inplace=True)
-        norm_df.set_index(keys=['symbol', 'contract'], drop=True, append=True, inplace=True)
-        norm_df.dropna(axis='index', inplace=True)
-
-        # save it
-        file_for_df = expression + '.pkl'
-        pathfile = os.path.join(path, file_for_df)
-        norm_df.to_pickle(pathfile)
+    return all_df
 
 
-def build_correlation_data():
+def clean_api_result(result):
+    # process result and return a df if possible; but it will be pivotted
+    df = process_chart_data(result, already_pivotted=True)
+    # so unpivot the data
+    norm_df = pd.melt(df, value_vars=df.columns, ignore_index=False)
+    # clean it
+    norm_df[['symbol', 'contract']] = norm_df['variable'].str.split(pat='|').apply(
+        lambda x: [e.strip() for e in x]).tolist()
+    norm_df.drop(columns='variable', inplace=True)
+    norm_df.set_index(keys=['symbol', 'contract'], drop=True, append=True, inplace=True)
+    norm_df.dropna(axis='index', inplace=True)
+    return norm_df
+
+
+def get_all_keys():
     all_keys_dict = {}
-    all_df = pd.DataFrame()
     for expression, type_ in trader_curves:
         all_keys_dict[expression] = {}
-        file_for_df = expression + '.pkl'
-        pathfile = os.path.join(path, file_for_df)
-        df = pd.read_pickle(pathfile)
-
         # get keys
         multiindex = list(df.index)
         multiindex = [(i[1], i[2]) for i in multiindex]  # sorry
         keys = list(set(multiindex))
         all_keys_dict[expression] = keys
+    return all_keys_dict
 
-        # one big df with all the lovely data
-        all_df = pd.concat([all_df, df], axis='index')
 
-    # from dict to list of lists
-    all_keys = [all_keys_dict[expression] for expression in all_keys_dict.keys()]
-    # create pairs eg selecting two from four products results in six pairs
-    corr_list = list(itertools.combinations(all_keys, 2))
-    charts_dict = {}
-    for product_x, product_y in corr_list:
-        # then for each of the pairs we need a cartesian product of spreads
-        # eg if each has ten months of spreads then we get 100 correlations to check
-        charts = list(itertools.product(product_x, product_y))
-        for chart in charts:
-            x, y = chart
-            x_symbol, x_contract = x
-            x_data = all_df.loc[slice(None), x_symbol, x_contract]
+def calc_correlation(df):
+    corr_matrix = df.corr(method='pearson', min_periods=1)
+    corr_scalar = np.dot(np.ones([1, 2]), corr_matrix.values).flatten()[0]
+    return df
 
-            y_symbol, y_contract = y
-            y_data = all_df.loc[slice(None), y_symbol, y_contract]
 
-            chart_df = pd.merge(x_data, y_data, how='inner', left_index=True, right_index=True, suffixes=['_x', '_y'])
-
-            key = ((x_symbol, x_contract), (y_symbol, y_contract))
-            charts_dict[key] = {}
-            charts_dict[key]['df'] = chart_df
-            charts_dict[key]['shape'] = chart_df.shape
-            corr_matrix = chart_df.corr(method='pearson', min_periods=1)
-            corr_scalar = np.dot(np.ones([1, 2]), corr_matrix.values).flatten()[0]
-            charts_dict[key]['pearson'] = corr_scalar
-
-    return charts_dict
+def build_correlation_data(df):
+    df.reset_index(['symbol', 'contract'], drop=False, inplace=True)
+    chart_df = pd.merge(df, df, how='inner',
+                        left_index=True, right_index=True,
+                        suffixes=['_x', '_y'])
+    return chart_df
 
 
 if __name__ == '__main__':
     env = DEV
-    #build_and_save_clean_data(trader_curves)
-    charts_dict = build_correlation_data()
+    pathfile = os.path.join(path, file_for_df)
 
-    for key, values in charts_dict.items():
-        print(key, values['pearson'])
-    pass
+    build_and_save = True
+
+    if build_and_save:
+        # build the data
+        df = collect_and_build_clean_data(trader_curves, start='2021-01-01', periods=2)
+        df.to_pickle(pathfile)
+
+    else:
+        # load the data
+        df = pd.read_pickle(pathfile)
+
+    cartesian_product_df = build_correlation_data(df)
+    cartesian_product_df = calc_correlation(cartesian_product_df)
+
 
