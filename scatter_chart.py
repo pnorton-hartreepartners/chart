3030c76534898e926d77aeb62f78573683d682aa
Norton <PNorton@hartreepartners.com>
2022-03-15 15:50:01 +0000

    wip

--- scatter_chart.py
+++ scatter_chart.py
@@ -5,9 +5,7 @@ import datetime as dt
 
 pkl_data_filepath = r'c:\temp\getTraderCurveTS.pkl'
 df = pd.read_pickle(pkl_data_filepath)
-
-x = '[RBOB FUTURE Apr22/May22]'
-y = '[RBOB FUTURE May22/Jun22/Jul22 fly]'
+x, y = df.columns
 
 start_date = dt.date(2022, 2, 1)
 start_date = pd.to_datetime(start_date)
@@ -27,7 +25,7 @@ min_marker_size = 5
 max_marker_size = 15
 marker_sizes = ((time_in_sec - min_sec) * (max_marker_size - min_marker_size) / (max_sec - min_sec)) + min_marker_size
 marker_sizes = marker_sizes.to_list()
-marker_sizes[-1] = 0
+#marker_sizes[-1] = 0
 
 layout = go.Layout(showlegend=True)
 fig2 = go.Figure(layout=layout)
@@ -44,12 +42,14 @@ marker_trace = go.Scatter(x=filtered_df[x], y=filtered_df[y],
                           text=filtered_df['time'].dt.strftime('%d-%b-%Y').values,
                           )
 line_trace = go.Scatter(x=filtered_df[x], y=filtered_df[y],
-                          mode='lines'
+                          mode='lines',
+                          showlegend=False
                           )
 now_trace = go.Scatter(x=[filtered_df.iloc[-1][x]], y=[filtered_df.iloc[-1][y]],
+                       name='now',
                        mode='markers',
                        marker_symbol='triangle-up',
-                       'size')
+                       marker_size=marker_sizes)
 fig2.add_trace(marker_trace)
 fig2.add_trace(line_trace)
 fig2.add_trace(now_trace)
